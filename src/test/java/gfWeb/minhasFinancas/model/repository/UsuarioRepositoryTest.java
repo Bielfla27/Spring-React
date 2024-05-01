8a89ef82eba3e568235444263e1b7b8987b313f6
Bielfla27 <gabibielmft@outlook.com>
2024-05-01 18:11:47 -0300

    Configuração do banco em memoria para testes

--- src/test/java/gfWeb/minhasFinancas/model/repository/UsuarioRepositoryTest.java
+++ src/test/java/gfWeb/minhasFinancas/model/repository/UsuarioRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import gfWeb.minhasFinancas.model.entity.Usuario;
@@ -13,6 +14,7 @@ import gfWeb.minhasFinancas.model.entity.Usuario;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
 public class UsuarioRepositoryTest {
 
 	@Autowired
